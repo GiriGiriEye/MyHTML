a035feb3214b1de23e0ad6b593e9049e20d007b4
ZhaoHongru <635351883@qq.com>
2020-08-31 17:16:43 +0800

    Add files via upload

--- zhr.js
+++ zhr.js
@@ -77,7 +77,7 @@ function initHighLevelWidget(){
 	// 加载 waifu.css live2d.min.js waifu-tips.js
 	if (screen.width >= 768) {
 		Promise.all([
-			loadExternalResource("https://raw.githubusercontent.com/GiriGiriEye/MyHTML/master/zhr.css", "css"),
+			loadExternalResource("https://cdn.jsdelivr.net/gh/GiriGiriEye/MyHTML@master/zhr.css", "css"),
 			loadExternalResource("https://cdn.jsdelivr.net/npm/font-awesome/css/font-awesome.min.css", "css"),
 			loadExternalResource(live2d_path + "live2d.min.js", "js"),
 			loadExternalResource(live2d_path + "waifu-tips.js", "js")
